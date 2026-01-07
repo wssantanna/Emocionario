9d37ebbfb3efc35068c4da29f71511c280133698
wssantanna <willian.santanna@nomeiodigital.com>
2026-01-07 00:18:32 -0300

    correção os retornos do 'Repository' para Usuário

--- src/Emocionario.Application/Usuarios/Repositories/IUsuarioRepository.cs
+++ src/Emocionario.Application/Usuarios/Repositories/IUsuarioRepository.cs
@@ -1,12 +1,12 @@
-﻿using Emocionario.Domain.Usuarios;
+using Emocionario.Domain.Usuarios;
 using Emocionario.Domain.Usuarios.ValueObjects;
-namespace Emocionario.Application;
+namespace Emocionario.Application.Usuarios.Repositories;
 public interface IUsuarioRepository
 {
     Task<bool> ExisteComEmailAsync(Email email, CancellationToken cancellationToken = default);
-    Task<Usuario> AdicionarAsync(Usuario usuario, CancellationToken cancellationToken = default);
+    Task AdicionarAsync(Usuario usuario, CancellationToken cancellationToken = default);
     Task<Usuario?> ObterPorIdAsync(Guid id, CancellationToken cancellationToken = default);
     Task<Usuario?> ObterPorEmailAsync(Email email, CancellationToken cancellationToken = default);
-    Task<bool> AtualizarAsync(Usuario usuario, CancellationToken cancellationToken = default);
+    Task AtualizarAsync(Usuario usuario, CancellationToken cancellationToken = default);
     Task<bool> ExcluirAsync(Guid id, CancellationToken cancellationToken = default);
 }
