01e184235042cd881d41afb5689f0bedfd86c5d4
wssantanna <willian.santanna@nomeiodigital.com>
2026-01-07 00:12:51 -0300

    ajuste para tornar o campo data nascimento opcional

--- src/Emocionario.Domain/Usuarios/Usuario.cs
+++ src/Emocionario.Domain/Usuarios/Usuario.cs
@@ -1,15 +1,15 @@
-namespace Emocionario.Domain.Usuarios;
 using Emocionario.Domain.Usuarios.ValueObjects;
+namespace Emocionario.Domain.Usuarios;
 public sealed class Usuario {
     public Guid Id { get; private set; }
     public PrimeiroNome Nome { get; private set; }
     public Sobrenome Sobrenome { get; private set; }
     public Email Email { get; private set; }
-    public DataNascimento DataNascimento { get; private set; }
+    public DataNascimento? DataNascimento { get; private set; }
     public DateTime CriadoEm { get; private set; }
     public DateTime? AtualizadoEm { get; private set; }
     private Usuario() { }
-    public static Usuario Criar(PrimeiroNome nome, Sobrenome sobrenome, Email email, DataNascimento dataNascimento)
+    public static Usuario Criar(PrimeiroNome nome, Sobrenome sobrenome, Email email, DataNascimento? dataNascimento = null)
     {
         return new Usuario
         {
@@ -21,7 +21,7 @@ public sealed class Usuario {
             CriadoEm = DateTime.UtcNow
         };
     }
-    public void Atualizar(PrimeiroNome nome, Sobrenome sobrenome, DataNascimento dataNascimento)
+    public void Atualizar(PrimeiroNome nome, Sobrenome sobrenome, DataNascimento? dataNascimento)
     {
         Nome = nome;
         Sobrenome = sobrenome;
