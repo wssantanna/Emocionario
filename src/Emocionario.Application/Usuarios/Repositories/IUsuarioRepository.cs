8fa9337c9281c19641836fb6e75804d540b30bc3
wssantanna <willian.santanna@nomeiodigital.com>
2026-01-06 23:15:02 -0300

    implementação de um contrato 'Repository' para Usuário

---
+++ src/Emocionario.Application/Usuarios/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using Emocionario.Domain.Usuarios;
+using Emocionario.Domain.Usuarios.ValueObjects;
+namespace Emocionario.Application;
+public interface IUsuarioRepository
+{
+    Task<bool> ExisteComEmailAsync(Email email, CancellationToken cancellationToken = default);
+    Task<Usuario> AdicionarAsync(Usuario usuario, CancellationToken cancellationToken = default);
+    Task<Usuario?> ObterPorIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<Usuario?> ObterPorEmailAsync(Email email, CancellationToken cancellationToken = default);
+    Task<bool> AtualizarAsync(Usuario usuario, CancellationToken cancellationToken = default);
+    Task<bool> ExcluirAsync(Guid id, CancellationToken cancellationToken = default);
+}
